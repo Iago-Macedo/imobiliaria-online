1bc7ae6520edca72b5d56a8f7f6241f41c243214
Evelyn Hilthegarden <142934215+EvelynHilthegarden@users.noreply.github.com>
2023-10-16 19:16:28 -0300

    Transformação da classe Cliente em Subclasse

--- ImobiliariaOnline/src/Cliente.java
+++ ImobiliariaOnline/src/Cliente.java
@@ -1,25 +1,5 @@
-public class Cliente {
-    private String numeroProcesso;
-    private String chaveDeAcesso;
-
-    public Cliente(String numeroProcesso, String chaveDeAcesso){
-        this.numeroProcesso = numeroProcesso;
-        this.chaveDeAcesso = chaveDeAcesso;
-    }
-
-    public void setChaveDeAcesso(String chaveDeAcesso) {
-        this.chaveDeAcesso = chaveDeAcesso;
-    }
-
-    public void setNumeroProcesso(String numeroProcesso) {
-        this.numeroProcesso = numeroProcesso;
-    }
-
-    public String getNumeroProcesso() {
-        return numeroProcesso;
-    }
-
-    public String getChaveDeAcesso() {
-        return chaveDeAcesso;
+public class Cliente extends Login {
+    public Cliente(String numeroProcesso, String chaveDeAcessoCliente){
+        super(numeroProcesso, chaveDeAcessoCliente);
     }
 }
